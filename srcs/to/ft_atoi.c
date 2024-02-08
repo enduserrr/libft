1dadeafcc243fce3d0851d690ef6460877fed16a
AIS <arttuilamarisalo@gmail.com>
2024-02-08 12:14:50 +0200

    now atoi deals with longs too

--- srcs/to/ft_atoi.c
+++ srcs/to/ft_atoi.c
@@ -6,17 +6,17 @@
 /*   By: asalo <asalo@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 11:25:18 by asalo             #+#    #+#             */
-/*   Updated: 2024/01/27 19:51:11 by asalo            ###   ########.fr       */
+/*   Updated: 2024/02/08 12:03:43 by asalo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+long	ft_atoi(const char *str)
 {
-	int			i;
-	int			sign;
-	long long	result;
+	int		i;
+	int		sign;
+	long	result;
 
 	i = 0;
 	sign = 1;
@@ -30,7 +30,7 @@ int	ft_atoi(const char *str)
 	}
 	else if (str[i] == '+')
 		i++;
-	while (str[i] && ('0' <= str[i]) && (str[i] <= '9'))
+	while (ft_isdigit(str[i]))
 	{
 		result = (result * 10) + (str[i] - '0');
 		i++;
